02125b61e9b9d9c2260c82684fa5444254819586
nikhil2898 <nikhilsaimuthyam1@gmail.com>
2025-11-16 17:20:55 +0530

    Update axios base URL to use environment variable

--- mern_chat/src/api/axios.js
+++ mern_chat/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // backend URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000", // backend URL
   withCredentials: true, // very important for cookies
 });
 
